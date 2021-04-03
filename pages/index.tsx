7835db4eaef1199e04e74fcfe7d552989b46d3de
Paul Lorenc <pjlorenc@gmail.com>
2021-04-03 09:12:12 -0400

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
-import { Container } from "@/components/Container";
+import Hero  from "@/components/Hero";
+
 export default function Home() {
   return (
-    <Container>
-      <h1 className="font-serif text-2xl font-extrabold">Zipperfield</h1>
-    </Container>
+    <div className="Wrapper">
+      <div className="Main-Container">
+        <h1 className="Heading">Under Maintenance</h1>
+        <Hero/>
+      </div> 
+   </div>
   );
 }
