ec41b5b5c9c6dd9ead6f9be855e85ac6d22a8d0e
Paul Lorenc <pjlorenc@gmail.com>
2021-04-03 09:31:04 -0400

    added responsive behavior

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Hero  from "@/components/Hero";
 export default function Home() {
   return (
     <div className="Wrapper">
-      <div className="Main-Container">
+      <div className="Main-Container w80 ">
         <h1 className="Heading">Under Maintenance</h1>
         <Hero/>
       </div> 
