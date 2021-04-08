e8c347abe058fdb64827f2ae182b109fe73257a8
Paul Lorenc <pjlorenc@gmail.com>
2021-04-07 21:55:00 -0400

    added text and email link

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,8 @@ export default function Home() {
     <div className="Wrapper">
       <div className="Main-Container w80 ">
         <h1 className="Heading">Under Maintenance</h1>
+        <p>Zipperfield: digital design and web development firm.</p>
+        <a className="link" href="mailto:hello@zipperfield.com">hello@zipperfield.com</a>
         <Hero/>
       </div> 
    </div>
